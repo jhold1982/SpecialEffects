948e6c05024776fe50a392e188d89f519e3129cf
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-04-12 10:37:02 -0500

    Initial Commit

---
+++ SpecialEffects/SpecialEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpecialEffectsApp.swift
+//  SpecialEffects
+//
+//  Created by Justin Hold on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SpecialEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
